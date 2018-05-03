b66d45854d175b3e1e6694c32d80c0b486c1fc71
Mateusz Tomaszewski <>
2018-05-03 23:47:14 +0200

    + kata

--- BowlingGame/BowlingGame/Game.cs
+++ BowlingGame/BowlingGame/Game.cs
@@ -1,10 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace BowlingGame
 {
     public class Game
     {
-        public Game()
+        List<int> results = new List<int>();
+
+        public void Roll(int pins)
         {
+            results.Add(pins);
+        }
+
+        public int Score()
+        {
+            return results.EndGame().ToFrames().Take(10).Sum(f => f.Sum());
+        }
+    }
+
+    public static class ListExtensions 
+    {
+        public static IEnumerable<IEnumerable<int>> ToFrames(this IEnumerable<int> list)
+        {
+            yield return list.TakeFrame();
+
+            foreach(var frame in list.SkipFrame().ToFrames())
+                yield return frame;
+        }
+
+        public static IEnumerable<int> TakeFrame(this IEnumerable<int> list)
+        {
+            if (list.IsSpare() || list.IsStrike())
+                return list.Take(3);
+            else
+                return list.Take(2);
+        }
+
+        public static IEnumerable<int> SkipFrame(this IEnumerable<int> list)
+        {
+            if (list.IsStrike())
+                return list.Skip(1);
+
+            return list.Skip(2);
+        }
+
+        public static IEnumerable<int> EndGame(this IEnumerable<int> list)
+        {
+            return list.Concat(new int[21]);
+        }
+
+        public static bool IsSpare(this IEnumerable<int> list)
+        {
+            return list.Take(2).Sum() == 10;
+        }
+
+        public static bool IsStrike(this IEnumerable<int> list)
+        {
+            return list.First() == 10;
         }
     }
 }
--- BowlingGame/BowlingGame/Test.cs
+++ BowlingGame/BowlingGame/Test.cs
@@ -3,11 +3,71 @@ using System;
 namespace BowlingGame
 {
     [TestFixture()]
-    public class Test
+    public class GameTest
     {
-        [Test()]
-        public void TestCase()
+        Game game;
+
+        [SetUp]
+        public void SetUp()
         {
+            game = new Game();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            game = null;
+        }
+
+        [Test()]
+        public void GutterGame()
+        {
+            for (int i = 0; i < 20; i++)
+                game.Roll(0);
+            
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [Test()]
+        public void SinglePinGame()
+        {
+            for (int i = 0; i < 20; i++)
+                game.Roll(1);
+
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [Test()]
+        public void SpareGame()
+        {
+            game.Roll(5);
+            game.Roll(5); // spare
+
+            game.Roll(2);
+
+            Assert.AreEqual(14, game.Score());
+        }
+
+        [Test()]
+        public void NoSpareGame()
+        {
+            game.Roll(2);
+            game.Roll(5);
+
+            game.Roll(5);
+            game.Roll(2);
+
+            Assert.AreEqual(14, game.Score());
+        }
+
+        [Test()]
+        public void StrikeGame()
+        {
+            game.Roll(10);
+            game.Roll(1);
+            game.Roll(6);
+
+            Assert.AreEqual(24, game.Score());
         }
     }
 }
